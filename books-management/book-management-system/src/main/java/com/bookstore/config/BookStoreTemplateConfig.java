647f4f0428c9dc89f3118fd1c824a0910f190e76
veeru110 <>
2023-10-17 18:11:24 +0530

    comment email template changes

--- books-management/book-management-system/src/main/java/com/bookstore/config/BookStoreTemplateConfig.java
+++ books-management/book-management-system/src/main/java/com/bookstore/config/BookStoreTemplateConfig.java
@@ -3,15 +3,17 @@ package com.bookstore.config;
 import freemarker.template.TemplateExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.TimeZone;
 
-@Configuration
+//@Configuration
 public class BookStoreTemplateConfig {
 
-    @Bean
+    //@Bean
+    //@Primary
     public freemarker.template.Configuration freeMarkerTemplateConfig() throws IOException {
         freemarker.template.Configuration configuration = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_31);
         configuration.setDirectoryForTemplateLoading(Paths.get("resources/templates").toFile());
